19a0c7a32d83d06429d875477a5834175acfd8d4
Marouf <maroufibrahimi7264@gmail.com>
2023-06-06 20:56:38 -0700

    show an educational UI and use alert dialog

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.musicplayer;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
 import com.google.android.material.internal.TouchObserverFrameLayout;
 
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     List<Song> allSongs = new ArrayList<>();
     ActivityResultLauncher<String> storagePermissionLauncher;
     //final String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+    final String permission = Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +39,52 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setTitle(getResources().getString(R.string.app_name));
+
+        // recyclerview
+        recyclerView = findViewById(R.id.recyclerview);
+        storagePermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), granted -> {
+            if (granted) {
+                //fetch songs
+                fetchSongs();
+            } else {
+                userResponses();
+            }
+        });
+
+        //lunch storage permission on create
+        storagePermissionLauncher.launch(permission);
     }
+
+    private void userResponses() {
+        if(ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED){
+            //fetch songs
+            fetchSongs();
+        }else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(shouldShowRequestPermissionRationale(permission)){
+                // show an educational UI user explaining why need this permission
+                // use alert dialog
+                new AlertDialog.Builder(this)
+                        .setTitle("Requesting Permission")
+                        .setMessage("Allow us to fetch songs")
+                        .setPositiveButton("allow", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                storagePermissionLauncher.launch(permission);
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getApplicationContext(), "You denied us to show songs", Toast.LENGTH_SHORT).show();
+                                dialogInterface.dismiss();
+                            }
+                        }).show();
+            }
+        }else{
+            Toast.makeText(this, "You canceled to show songs", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void fetchSongs() {
+    }
+
 }
